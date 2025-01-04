edb62baf54dd8d6d7052c0991e2804eb69e27134
MohammadYehya <mohammad7446@gmail.com>
2025-01-04 20:19:56 +0500

    Update Dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { CamSelector } from "@/components/myui/camselector";
 import React from "react";
 import Webcam from "react-webcam";
 
-
 export default function Home() {
   const [useDevices, setUseDevices] = React.useState<MediaDeviceInfo[]>([]);
   const [devices, setDevices] = React.useState<MediaDeviceInfo[]>([]);
@@ -22,26 +21,50 @@ export default function Home() {
     [setDevices]
   );
 
+  const getGridSize = () => {
+    switch (Math.ceil(Math.sqrt(useDevices.length)))
+    {
+      case 1:
+        return ' grid-rows-1 grid-cols-1 ';
+      case 2:
+        return ' grid-rows-2 grid-cols-2 ';
+      case 3:
+        return ' grid-rows-3 grid-cols-3 ';
+      default:
+        return ' grid-rows-0 grid-cols-0 ';
+    }
+  }
+
   React.useEffect(() => {
     navigator.mediaDevices.enumerateDevices().then(handleAvailableDevices);
   }, [handleAvailableDevices]);
 
   return (
-    <div className="w-screen h-screen flex">
-      <div className={`grid grid-cols-12 grid-rows-12 w-4/5 bg-red-500 transition-all duration-500`}>
-      {/* ${useDevices.length} */}
+    <div className="w-screen h-screen flex text-white">
+      <div className={getGridSize() +` grid w-4/5 bg-gray-900 transition-all duration-500 `}>
+
         {
+          useDevices.length === 0 ?
+          <div className="flex justify-center items-center text-5xl font-black">
+            No Selected Camera Device!
+          </div>
+          :
+          useDevices.length > 9 ?
+          <div className="flex justify-center items-center text-5xl font-black">
+            Can not display more that 9 video screens!
+          </div>
+          :
           useDevices.map((device, key) => (
-            <div className={`col-span-1 row-span-1 bg-green-500 border-r-black border-r-8`} key={key}>
+            <div className={`flex col-span-1 row-span-1`} key={key}>
               <Webcam className="h-full" audio={false} videoConstraints={{ deviceId: device.deviceId , aspectRatio: (screen.width/screen.height)}} />
             </div>
           ))
         }
       </div>
 
-      <div className="w-1/5 h-full bg-blue-500">
-        <CamSelector devices={devices} handleDevices={handleDevices}/>
-        <button onClick={() => setUseDevices(useDevices.concat([devices[0]]))}>Test</button>
+      <div className="w-1/5 h-full flex flex-col items-center justify-between bg-gray-800 p-10">
+        <CamSelector className="flex justify-center items-center border h-8 w-full rounded-xl hover:bg-white hover:text-black transition-all hover:scale-110" devices={devices} handleDevices={handleDevices}/>
+        <button className="flex justify-center items-center border h-8 w-full rounded-xl hover:bg-white hover:text-black transition-all hover:scale-110" onClick={() => setUseDevices(useDevices.concat([devices[0]]))}>Test</button>
       </div>
     </div>
   );
--- src/components/myui/camselector.tsx
+++ src/components/myui/camselector.tsx
@@ -2,20 +2,20 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
+  // DropdownMenuLabel,
+  // DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-export function CamSelector({ devices, handleDevices } : { devices:MediaDeviceInfo[], handleDevices: (device: MediaDeviceInfo) => void}) {
+export function CamSelector({ devices, handleDevices, className=`` } : { devices:MediaDeviceInfo[], handleDevices: (device: MediaDeviceInfo) => void, className:string}) {
     return (
         <DropdownMenu>
-        <DropdownMenuTrigger>
-          Open
+        <DropdownMenuTrigger className={className}>
+        Select Available Cameras
         </DropdownMenuTrigger>
         <DropdownMenuContent>
-          <DropdownMenuLabel>Select Available Cameras</DropdownMenuLabel>
-          <DropdownMenuSeparator />
+          {/* <DropdownMenuLabel>Select Available Cameras</DropdownMenuLabel> */}
+          {/* <DropdownMenuSeparator /> */}
           {
             devices.length === 1 && devices[0].deviceId === '' ?
             <DropdownMenuItem>No Detected Devices</DropdownMenuItem>
