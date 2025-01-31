f2841469dd3e55cab1d570d10ba7a1badcb95fe0
MohammadYehya <mohammad7446@gmail.com>
2025-02-01 02:28:33 +0500

    Implemented Toast component

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,16 @@
+import { toast } from "@/hooks/use-toast"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function ErrorPopup(msg: string)
+{
+  return toast({
+    variant: "destructive",
+    title: "An Error Occured!",
+    description: msg
+  })
 }
