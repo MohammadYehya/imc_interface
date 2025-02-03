b03fe8a9af047522951b9cdbde5a1923d33f9cbb
MohammadYehya <mohammad7446@gmail.com>
2025-02-04 01:37:48 +0500

    Update camselector.tsx

--- frontend/src/components/myui/camselector.tsx
+++ frontend/src/components/myui/camselector.tsx
@@ -6,8 +6,9 @@ import {
     // DropdownMenuSeparator,
     DropdownMenuTrigger,
   } from "@/components/ui/dropdown-menu";
+import { WebcamInfo } from "@/lib/utils";
   
-  export function CamSelector({ devices, handleDevices, className=`` } : { devices:MediaDeviceInfo[], handleDevices: (device: MediaDeviceInfo) => void, className:string}) {
+  export function CamSelector({ devices, handleDevices, className=`` } : { devices:WebcamInfo[], handleDevices: (device: MediaDeviceInfo) => void, className:string}) {
       return (
           <DropdownMenu>
           <DropdownMenuTrigger className={className}>
@@ -17,10 +18,10 @@ import {
             {/* <DropdownMenuLabel>Select Available Cameras</DropdownMenuLabel> */}
             {/* <DropdownMenuSeparator /> */}
             {
-              devices.length === 1 && devices[0].deviceId === '' ?
+              devices.length === 1 && devices[0].MediaData.deviceId === '' ?
               <DropdownMenuItem>No Detected Devices (either due to insufficient permissions or no connected devices)</DropdownMenuItem>
               :
-              devices.map((device, key) => <DropdownMenuItem key = {key} onClick={() => handleDevices(device)}>{ device.label }</DropdownMenuItem>)
+              devices.map((device, key) => <DropdownMenuItem key = {key} onClick={() => handleDevices(device.MediaData)}>{ device.CamLabel }</DropdownMenuItem>)
             }
           </DropdownMenuContent>
         </DropdownMenu>
