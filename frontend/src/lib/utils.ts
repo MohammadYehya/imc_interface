0226e01a95a8347df03928742d24bddc4666eb15
MohammadYehya <mohammad7446@gmail.com>
2025-02-04 01:37:41 +0500

    Moved Webcam interface

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { toast } from "@/hooks/use-toast"
 import { clsx, type ClassValue } from "clsx"
+import Webcam from "react-webcam"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -13,4 +14,10 @@ export function ErrorPopup(msg: string)
     title: "An Error Occured!",
     description: msg
   })
+}
+
+export interface WebcamInfo {
+  MediaData: MediaDeviceInfo;
+  WebcamRef: React.RefObject<Webcam | null>;
+  CamLabel: string;
 }
