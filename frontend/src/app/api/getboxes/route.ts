4a10c85d8f276dcf89a8b6993ee0c15bfe00ea95
MohammadYehya <mohammad7446@gmail.com>
2025-02-23 22:39:01 +0500

    Create route.ts

---
+++ frontend/src/app/api/getboxes/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest } from "next/server";
+
+const PATH = 'localhost'
+
+export async function POST(req: NextRequest) {
+  const file = await req.json()
+  const resp = await fetch(`http://${PATH}:8000/predict/${req.nextUrl.searchParams.get('cam_id')}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(file),
+    });
+  return resp;
+}
