fc609ab9a7e2bf86635e1f39576bdf58a59b00b2
MohammadYehya <mohammad7446@gmail.com>
2025-02-23 22:38:57 +0500

    Created new service for Boxes

---
+++ boundingbox_servcice/main.py
@@ -0,0 +1,36 @@
+from ultralytics import YOLO
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+import base64
+import io
+
+model = YOLO("./best.pt")
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],    
+)
+
+@app.get('/')
+def home():
+    return ''
+
+@app.post('/predict/{cam_id}')
+async def get(cam_id: str, file: Request):
+    file = (await file.json())
+    if file == {} or file == {'image':None}:
+        return JSONResponse(content='Empty Image Sent!', status_code=415, media_type='application/json')
+    file = file['image']
+    if "data:image" in file:
+        file = file.split(",")[1]
+    img = base64.b64decode(file)
+    img = Image.open(io.BytesIO(img))
+    results = model(img, verbose=False)
+    return results[0].boxes.xywhn[0].tolist()
