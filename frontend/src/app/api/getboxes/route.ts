c3052f2d48682b4614e23e452cfdfdf76dfdffc8
MohammadYehya <mohammad7446@gmail.com>
2025-02-26 14:06:34 +0500

    Refactor

--- frontend/src/app/api/getboxes/route.ts
+++ frontend/src/app/api/getboxes/route.ts
@@ -1,10 +1,10 @@
 import { NextRequest } from "next/server";
 
-const PATH = 'localhost'
+// const PATH = 'localhost'
 
 export async function POST(req: NextRequest) {
   const file = await req.json()
-  const resp = await fetch(`http://${PATH}:8000/predict/${req.nextUrl.searchParams.get('cam_id')}`, {
+  const resp = await fetch(`http://${process.env.SERVICE2_PATH || 'localhost'}:8000/predict/${req.nextUrl.searchParams.get('cam_id')}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(file),
--- frontend/src/app/api/predict/route.ts
+++ frontend/src/app/api/predict/route.ts
@@ -1,14 +1,14 @@
 import { NextRequest } from "next/server";
 
-const PATH = 'aimodel_service'
+// const PATH = 'aimodel_service'
 
 export async function GET() {
-  const res = await fetch(`http://${PATH}:8000`);
+  const res = await fetch(`http://${process.env.SERVICE1_PATH || 'localhost'}:8000`);
   return res;
 }
 export async function POST(req: NextRequest) {
   const file = await req.json()
-  const resp = await fetch(`http://${PATH}:8000/predict/${req.nextUrl.searchParams.get('cam_id')}`, {
+  const resp = await fetch(`http://${process.env.SERVICE1_PATH || 'localhost'}:8000/predict/${req.nextUrl.searchParams.get('cam_id')}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(file),
