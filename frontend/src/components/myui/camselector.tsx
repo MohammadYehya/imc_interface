cc47c2b0fd2a6493440f11a70ef5b37e5072a825
MohammadYehya <mohammad7446@gmail.com>
2025-03-02 22:41:30 +0500

    Refactored to Avoid Memory Leaks

--- frontend/src/components/myui/camselector.tsx
+++ frontend/src/components/myui/camselector.tsx
@@ -8,7 +8,7 @@ import {
   } from "@/components/ui/dropdown-menu";
 import { WebcamInfo } from "@/lib/utils";
   
-  export function CamSelector({ devices, handleDevices, className=`` } : { devices:WebcamInfo[], handleDevices: (device: MediaDeviceInfo) => void, className:string}) {
+  export function CamSelector({ devices, handleDevices, className=`` } : { devices:WebcamInfo[], handleDevices: (device: WebcamInfo) => void, className:string}) {
       return (
           <DropdownMenu>
           <DropdownMenuTrigger className={className}>
@@ -21,7 +21,7 @@ import { WebcamInfo } from "@/lib/utils";
               devices.length === 1 && devices[0].MediaData.deviceId === '' ?
               <DropdownMenuItem>No Detected Devices (either due to insufficient permissions or no connected devices)</DropdownMenuItem>
               :
-              devices.map((device, key) => <DropdownMenuItem key = {key} onClick={() => handleDevices(device.MediaData)}>{ device.CamLabel }</DropdownMenuItem>)
+              devices.map((device, key) => <DropdownMenuItem key = {key} onClick={() => handleDevices(device)}>{ device.CamLabel }</DropdownMenuItem>)
             }
           </DropdownMenuContent>
         </DropdownMenu>
